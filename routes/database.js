a7ce72dfffecbf9d6420fe29e7d1fbbf53347a25
sheoplee <124544275+sheoplee@users.noreply.github.com>
2023-02-09 23:32:37 +0900

    Delete database.js

--- routes/database.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    user     :process.env.NODE_ORACLEDB_USER || "java",
-    password :process.env.NODE_ORACLEDB_PASSWORD || "oracle",
-    connectString :process.env.NODE_ORACLEDB_CONNECTSTRING || "localhost:1521/orcl",
-    externalAuth  :process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
-}
